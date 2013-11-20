fcd11c9d8a111bda908e7faa943c8e28841139df
thomas.pouncy@gmail.com <thomas.pouncy@gmail.com>
2013-11-20 13:34:16 -0500

    switched to oauth 2.0 for authentication

--- lib/giggly/rest/request.rb
+++ lib/giggly/rest/request.rb
@@ -2,15 +2,15 @@ module Giggly
   module Rest
     class Request
       include HTTParty
-      attr_accessor :api_key, :secret_key, :uid
+      attr_accessor :api_key, :secret_key, :uid, :access_token
       format :xml
-      
+
       headers 'Content-Length' => '0'
-      
+
       # Accepts a hash of connection parameters that will be used to authenticate requests with gigya
-      # and specify the user that the request is specific to. The keys for the has are all symbols. 
+      # and specify the user that the request is specific to. The keys for the has are all symbols.
       # The connection parameter hash requires :api_key, :secret_key, and :uid
-      # These are the same parameters that can be passed to the constructor for +Giggly::Rest::Socialize+ 
+      # These are the same parameters that can be passed to the constructor for +Giggly::Rest::Socialize+
       # example:
       #    @connection = Giggly::Rest::Request.new(
       #      :api_key    => 'api key provided from Gigya',
@@ -19,49 +19,54 @@ module Giggly
       #    )
       def initialize(conn_params)
         @api_key      = conn_params[:api_key]
-        @secret_key   = conn_params[:secret_key]
-        @uid          = conn_params[:user_id]
-        @gigya_secret = Base64.decode64(@secret_key)
+        @secret_key   = conn_params[:secret_key] unless conn_params[:secret_key].blank?
+        @uid          = conn_params[:user_id] unless conn_params[:user_id].blank?
+        @access_token = conn_params[:access_token] unless conn_params[:access_token].blank?
+        raise "Invalid Request: Need secret key or access token" if @secret_key.nil? && @access_token.nil?
       end
 
       # Wraps around HTTParty's post method to make API calls.
       # Responsible for raising errors if they are returned from the API.
       # Returns response data from the post request.
       def post(url, params = {})
-        response = self.class.post(url, :query => sign('POST', url, params))
+        if @secret_key
+          response = self.class.post(url, :query => params.merge({:client_id=>@api_key,:client_secret=>@secret_key}))
+        elsif @access_token
+          response = self.class.post(url, :query => params.merge({:oauth_token=>@secret_key}))
+        end
         response_key, response_data = response.shift
         raise_errors(response_data)
         response_data
       end
-      
-      def sign(http_method, api_url, params)
-        params.merge! "apiKey" => @api_key, "uid" => @uid
-        params.merge  "sig" => signature(http_method, api_url, params)
-      end
-      
-      def signature(http_method, api_url, params)
-        timestamp = Time.now.to_i.to_s
-        params.merge!("nonce" => "#{@uid}#{timestamp}", "timestamp" => timestamp) ####
-        base_string = build_base_string(http_method, api_url, params)
-        hmacsha1 = HMAC::SHA1.digest(@gigya_secret, base_string)
-        sig = Base64.encode64(hmacsha1).chomp.to_s.gsub(/\n/,'')
-      end
-      
+      # 
+      # def sign(http_method, api_url, params)
+      #   params.merge! "apiKey" => @api_key, "uid" => @uid
+      #   params.merge  "sig" => signature(http_method, api_url, params)
+      # end
+      # 
+      # def signature(http_method, api_url, params)
+      #   timestamp = Time.now.to_i.to_s
+      #   params.merge!("nonce" => "#{@uid}#{timestamp}", "timestamp" => timestamp) ####
+      #   base_string = build_base_string(http_method, api_url, params)
+      #   hmacsha1 = HMAC::SHA1.digest(@gigya_secret, base_string)
+      #   sig = Base64.encode64(hmacsha1).chomp.to_s.gsub(/\n/,'')
+      # end
+
       private
 
         # what is this supposed to do? or when is it supposed to be used?
-        def validate_signature(api_url, params, sig)
-          sig === signature(api_url, params) 
-        end
-        
+        # def validate_signature(api_url, params, sig)
+        #   sig === signature(api_url, params)
+        # end
+
         def params_to_string(params)
           params.sort {|a,b| a[0].to_s <=> b[0].to_s}.collect {|a| "#{a[0]}=#{CGI.escape a[1].to_s}"}.join('&').gsub('+', '%20')
         end
-        
+
         def build_base_string(http_method, api_url, params)
           [http_method, api_url, params_to_string(params)].collect! {|a| CGI.escape a}.join('&')
         end
-      
+
         def raise_errors(data)
           return if '200' == data["statusCode"]
           case data["statusCode"].to_i
@@ -83,7 +88,7 @@ module Giggly
               raise Giggly::Rest::NotImplemented.new(data)
           end
         end
-            
+
     end
   end
 end
--- lib/giggly/rest/socialize.rb
+++ lib/giggly/rest/socialize.rb
@@ -1,11 +1,11 @@
 module Giggly
   module Rest
     class Socialize
-      
+
       GIGYA_URL = "http://socialize.api.gigya.com/socialize."
-      
+
       attr_accessor :request
-      
+
       # Create a new socialize object by passing in a <tt>Giggly::Rest::Socialize</tt> object
       # or by passing in a hash that will be used to initialize a new <tt>Giggly::Rest::Socialize</tt>.
       # See the rdoc for Request to find details for a new connection hash.
@@ -17,6 +17,11 @@ module Giggly
         @request = Giggly::Rest::Request.new(request)
       end
 
+      def self.get_token(api_key,secret,uid)
+        @request = Giggly::Rest::Request.new(:api_key=>api_key,:secret=>secret)
+        @request.post "#{GIGYA_URL}get_token", {:grant_type=>"none",:x_siteUID=>uid}
+      end
+
       # Disconnects the user from the provided network.
       # Params::
       # * +network+ array of networks to disconnect from, if emtpy user will be disconnected from all networks
@@ -25,10 +30,10 @@ module Giggly
         validate_providers! %w[facebook yahoo myspace twitter], provider
         perform_post :disconnect, :provider => (provider.join(',') if provider)
       end
-      
+
       # retrieves the friends of the current user
       # Params::
-      # * +enabled_providers+ comma +separated+ list of providers to include.  (ONLY) 
+      # * +enabled_providers+ comma +separated+ list of providers to include.  (ONLY)
       # * +disabled_providers+ comma separated list of providers to exclude. (NOT)
       # * +uids+ list of users to retrieve.
       # * +detail_level+ 'basic' or 'extended'
@@ -38,7 +43,7 @@ module Giggly
         validate_providers! %w[facebook yahoo myspace twitter], options[:enabled_providers]
         params = provider_hash(options)
         params[:user_ids], params[:detail_level] = options[:uids], options[:detailLevel]
-        
+
         response = perform_post(:getFriendsInfo, params)
         friends = []
         response['friends']['friend'].each do |friend|
@@ -46,7 +51,7 @@ module Giggly
         end
         friends
       end
-      
+
       # get raw data from an individual provider about a user
       # Params::
       # * +provider+ the provider to retrieve the raw data from, only facebook and myspace are currently supported
@@ -57,12 +62,12 @@ module Giggly
         validate_providers! %w[facebook myspace], [provider]
         perform_post :getRawData, {:provider => provider, :fields => fields.join(',')}
       end
-      
+
       # get the connection info for a session with a direct api provider
       # this is useful to make calls to a specific provider (not via Socialize)
       # for functions that are currently unsupported by Gigya Socialize
       # Params::
-      # * +provider+ the provider to get the connection information for, 
+      # * +provider+ the provider to get the connection information for,
       #   * possible values are facebook, myspace, twitter, or yahoo
       # * +padding_mode+ padding mode for the AES algorithm used in encryping some of the response parameters
       #   * values are PKCS5, PKCS7 or ZEROS PKCS7 will be used as the default
@@ -72,24 +77,24 @@ module Giggly
         # TODO: possibly decrypt response
         Giggly::SessionInfo.new perform_post(:getSessionInfo, {:provider => provider, :paddingMode => padding_mode})
       end
-      
+
       # retrieves user information from gigya, including or excluding providers
       # as indicated.  default usage includes all providers.  returns user.
       # Params::
-      # * +providers+ an optional hash of arrays the has the keys of 
-      # * +enabled_providers+ an array of provider strings 
-      # * +disabled_providers+ an array of provider strings 
+      # * +providers+ an optional hash of arrays the has the keys of
+      # * +enabled_providers+ an array of provider strings
+      # * +disabled_providers+ an array of provider strings
       def user_info(providers = {})
         Giggly::User.new perform_post(:getUserInfo, provider_hash(providers))
       end
-      
+
       # see: http://wiki.gigya.com/030_Gigya_Socialize_API_2.0/030_API_reference/REST_API/socialize.publishUserAction
       # for how to create user_action_xml
       def publish_user_action(user_action_xml, providers = {})
-        validate_providers! %w[facebook yahoo], providers[:enabled_providers]
+        # validate_providers! %w[facebook yahoo], providers[:enabled_providers]
         perform_post :publishUserAction, {:userAction => user_action_xml}.merge(provider_hash(providers))
       end
-      
+
       # Sends a notification to a list of friends
       # Params::
       # * +recipients+ a string or array of uids to send the notification to
@@ -100,18 +105,18 @@ module Giggly
         recipients = recipients.is_a?(Array) ? recipients.join(',') : recipients
         perform_post :sendNotification, :recipients => recipients, :subject => subject, :body => body
       end
-      
+
       # sets the status of a user for the given providers (or all of them if blank)
       # Params::
       # * +status+ the status to set for the user
-      # * +providers+ an optional hash of arrays the has the keys of 
-      # * +enabled_providers+ an array of provider strings 
+      # * +providers+ an optional hash of arrays the has the keys of
+      # * +enabled_providers+ an array of provider strings
       # * +disabled_providers+ an array of provider strings
       def set_status(status, providers = {})
         validate_providers! %w[facebook yahoo myspace twitter], providers[:enabled_providers]
         perform_post :setStatus, {:status => status}.merge(provider_hash(providers))
       end
-      
+
       # sets the status of a user for all providers
       # Params::
       # * +status+ the status to set for the user
@@ -121,19 +126,19 @@ module Giggly
       def status=(status)
         set_status status
       end
-      
+
       protected
-      
+
         def perform_post(action, params = {})
           params.reject! {|k,v| v.nil?}
           @request.post "#{GIGYA_URL}#{action}", params
         end
-        
+
         def provider_hash(providers)
           { :enabledProviders  => (providers[:enabled_providers].join(',') if providers[:enabled_providers]),
             :disabledProviders => (providers[:disabled_providers].join(',') if providers[:disabled_providers]) }
         end
-        
+
         def validate_providers!(valid_providers, given_providers = nil)
           return if given_providers.nil?
           given_providers.each do |provider|
@@ -142,9 +147,9 @@ module Giggly
             ) unless valid_providers.include? provider
           end
         end
-        
+
         class InvalidProvider < StandardError; end
-      
+
     end
   end
 end
